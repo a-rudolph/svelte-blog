505173d3cd2523d1b8e3a197cb8550a47fb92d20
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-12-18 15:42:30 -0500

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	const title = 'hello world';
+
+	console.log(title);
+</script>
+
+<h1>{title}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

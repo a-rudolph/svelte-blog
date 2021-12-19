dd19b6235c1d710b82062d30a33e4e1f4d428b91
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2021-12-18 21:42:06 -0500

    quick cards

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,56 @@
 <script>
-	const title = 'hello world';
+  const title = 'This blog is built on sveltekit!';
 
-	console.log(title);
+  const numberOfPosts = 5;
+
+  const firstPost = {
+    title: 'How I made a blog website using sveltekit.',
+    body: 'I wanted to try the svelte framework so I decided to make a blog website. In order to use sveltekit I first needed to update npm.',
+  };
+
+  const posts = Array(numberOfPosts)
+    .fill(firstPost)
+    .map((_, i) => ({ ..._, id: i + 1 }));
 </script>
 
 <h1>{title}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="posts-container">
+  {#each posts as post (post.id)}
+    <div class="card">
+      <h3>{post.title}</h3>
+      <p>{post.body}</p>
+    </div>
+  {:else}
+    <p>no blog posts to show 🤷‍♂️</p>
+  {/each}
+</div>
+<p class="footer">
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  .footer {
+    position: absolute;
+    bottom: 0;
+  }
+
+  .posts-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 auto;
+    max-width: 964px;
+  }
+
+  .card {
+    padding: 24px;
+    margin: 16px;
+    width: 400px;
+
+    border: 1px solid rgb(0, 0, 0, 0.6);
+    border-radius: 2px;
+  }
+</style>

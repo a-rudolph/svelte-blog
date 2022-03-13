0cda40ea921d2377f371eb2b2cc9f2600db5c989
a-rudolph <adam.rudolph@live.ca>
2022-03-12 21:24:51 -0500

    Post component

---
+++ src/components/Post.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title;
+  export let date;
+  export let teaser;
+</script>
+
+<div class="pv-3">
+  <div class="flex justify-between items-baseline">
+    <h2 class="text-cyan-400 mb-3">{title}</h2>
+    <div>{date}</div>
+  </div>
+
+  <div class="text-justify">
+    {teaser}
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,31 @@
 <script>
+  import Post from '../components/Post.svelte';
 </script>
 
-<div class="container prose mx-auto px-4">
+<div class="container prose mx-auto px-4 pt-[40px]">
   <h1>ar-blog</h1>
 
-  <div class="flex m-1">
+  <div class="flex m-1 mb-5">
     <div
       class="rounded-full m-1 ml-[-4px]
    bg-slate-50 h-[48px] w-[48px]"
     />
-    <div class="p-1">
+    <div class="p-1 pl-3">
       Personal blog by <b>Adam Rudolph</b><br /> DX enthusiast
     </div>
   </div>
 
-  <h3>coder's palette</h3>
+  <Post
+    title={`coder's palette`}
+    date="March 12, 2022"
+    teaser={`know how to access all the tools at your disposal. easy to navigate and learn the shortcuts
+    you need.`}
+  />
 
-  <div>this is it</div>
+  <Post
+    title={`some other article`}
+    date="February 4, 2022"
+    teaser={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni similique laborum ratione
+      reiciendis ipsum ducimus.`}
+  />
 </div>

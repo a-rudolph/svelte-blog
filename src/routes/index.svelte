643c40dddbca2f20bad1d21eb419d308654b3747
a-rudolph <adam.rudolph@live.ca>
2022-03-12 20:39:06 -0500

    prose layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,20 @@
 <script>
-
 </script>
 
-<div>
-  this is it
+<div class="container prose mx-auto px-4">
+  <h1>ar-blog</h1>
+
+  <div class="flex m-1">
+    <div
+      class="rounded-full m-1 ml-[-4px]
+   bg-slate-50 h-[48px] w-[48px]"
+    />
+    <div class="p-1">
+      Personal blog by <b>Adam Rudolph</b><br /> DX enthusiast
+    </div>
+  </div>
+
+  <h3>coder's palette</h3>
+
+  <div>this is it</div>
 </div>

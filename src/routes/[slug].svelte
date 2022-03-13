54b48615bfd9a78e7b28d2799479ef7918808d19
a-rudolph <adam.rudolph@live.ca>
2022-03-12 22:27:02 -0500

    cleanup module load function

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -1,23 +1,9 @@
 <script context="module">
-  export async function load({ page }) {
+  export const load = ({ page }) => {
     const { slug } = page.params;
 
     return { props: { slug } };
-
-    //   let posts;
-    //   try {
-    //     const res = await fetch('http://localhost:3000/api/posts');
-    //     const body = await res.json();
-    //     posts = body.posts;
-    //   } catch (err) {
-    //     posts = [];
-    //   }
-    //   return {
-    //     props: {
-    //       posts,
-    //     },
-    //   };
-  }
+  };
 </script>
 
 <script>
